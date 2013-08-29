d98ac32fda6e86ee001b4bcbec96a46e56691f04
Harihar Shankar <hariharshankar@gmail.com>
2013-08-29 10:40:31 -0600

    added metadata update shell script

---
+++ src/main/resources/metadata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+case `uname` in
+  CYGWIN*)
+    CP=$( echo `dirname $0`/../lib/*.jar . | sed 's/ /;/g')
+    ;;
+  *)
+    CP=$( echo `dirname $0`/../lib/*.jar . | sed 's/ /:/g')
+esac
+
+# Find Java
+if [ "$JAVA_HOME" = "" ] ; then
+    JAVA="java -server"
+else
+    JAVA="$JAVA_HOME/bin/java -server"
+fi
+
+# Set Java options
+if [ "$JAVA_OPTIONS" = "" ] ; then
+    JAVA_OPTIONS="-Xms250M -Xmx250M"
+fi
+
+$JAVA $JAVA_OPTIONS -cp $CP:$CLASSPATH org.geo.core.utils.Metadata $@
